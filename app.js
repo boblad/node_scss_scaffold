c464c01c9fdf317cc5b02ff403a5678181519de9
Bryant Oblad <bryant.oblad@gmail.com>
2017-03-30 12:49:01 -0600

    adding in test websocket

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var hbs = require('hbs');
 var moment = require('moment');
+var http = require('http');
 var port = 3015;
 
+var WebSocket = require('ws');
+
 var app = express();
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 var ENV = process.env.NODE_ENV;
 var is_production = true;
@@ -29,12 +35,28 @@ app.enable('trust proxy');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  res.render('home', {
-    is_production: is_production
-  })
-})
+app.use(function (req, res) {
+    res.render('home', {
+      is_production: is_production
+    })
+});
 
-app.listen(port, function() {
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    console.log('hdhdh', client)
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+// use like this:
+wss.on('connection', function(ws) {
+  ws.on('message', function(message) {
+    wss.broadcast(message);
+  });
+});
+server.listen(port, function() {
   console.log('listening on port ', port)
 });
--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,34 @@
     $(function() {
 
       console.log('loaded');
-      
+
+// if user is running mozilla then use it's built-in WebSocket
+window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+// if browser doesn't support WebSocket, just show some notification and exit
+
+// open connection
+var connection = new WebSocket('ws://localhost:3015');
+
+connection.onopen = function () {
+    console.log('plsl')
+    console.log('con', connection)
+
+    $('#hi').on('input',function() {
+      connection.send($(this).val())
+    })
+};
+
+connection.onerror = function (error) {
+  console.log('err', error)
+};
+
+// most important part - incoming messages
+connection.onmessage = function (message) {
+  console.log('mes', message)
+  $('#hi').val(message.data)
+};
+
+
     });
 })(jQuery);
